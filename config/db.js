707dd3d65ce363918e9666b1dee2b1332a83fd18
qaimabbas <45366039+qaimabbas@users.noreply.github.com>
2020-10-23 18:56:38 +0200

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,12 @@
+/*
+const mongoose = require('mongoose');
+//const keys = require('./keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose
+  .connect('mongodb+srv://tanveer:tanveer@cluster0.jpspo.mongodb.net/<dbname>?retryWrites=true&w=majority', {useUnifiedTopology: true ,useNewUrlParser: true} )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err)); 
+  */
---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: '', 
+  appId: '1095512',
+  key: '1052adeaf088badd4230',
+  secret: '6e7126402e2a9d6628e1',
+  cluster: 'us2',
+  encrypted: true
+  };
