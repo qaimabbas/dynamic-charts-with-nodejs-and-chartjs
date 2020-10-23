3512f774e02c0933fa548d6636a82d5690996b82
qaimabbas <45366039+qaimabbas@users.noreply.github.com>
2020-10-23 18:58:39 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express= require('express');
+const bodyParser= require('body-parser');
+const path= require('path');
+const cors= require('cors');
+
+//db config require
+ //require('./config/db');
+
+
+const app = express();
+const poll=require('./routes/poll');
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+// Enable CORS
+app.use(cors());
+
+app.use('/poll', poll);
+
+const port = 4000;
+
+// Start server
+app.listen(port, () => console.log(`Server started on port ${port}`));
